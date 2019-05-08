42feaf9a89b33eef24c2610d9ddbce6acbba7ec9
P S <s.prerita@gmail.com>
2019-05-08 03:15:15 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+describe "fizzbuzz" do
+  it 'returns "Fizz" when the number is divisible by 3' do
+    fizz_3 = fizzbuzz(3)
+ 
+    expect(fizz_3).to eq("Fizz")
+  end
+
